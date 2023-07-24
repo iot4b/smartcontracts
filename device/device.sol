d85f95cf14eff94a6ca82816a6718e73510d852e
Alexey Spiridonov <forspall@gmail.com>
2023-07-24 19:53:48 +0700

    fix

--- device/device.sol
+++ device/device.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
 }
 
 contract Device {
