73c760050acb51f75219c3eccac508552bba96a4
Alexey Spiridonov <forspall@gmail.com>
2023-07-24 19:34:31 +0700

    device

--- device/device.sol
+++ device/device.sol
@@ -1,15 +1,23 @@
 pragma solidity ^0.8.0;
 
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
 contract Device {
-    address public currentNode;
     string public dtype;
+    string public status;
+    string public version;
+    address public currentNode;
     address public vendorContract;
+    address public electorContract;
     address public nodeContract;
     address public ownerContract;
-    string public status;
+    IERC20 public token;
 
-    constructor() {
+    constructor(IERC20 _token) {
         ownerContract = msg.sender;
+        token = _token;
     }
 
     modifier onlyNodeContract() {
@@ -17,11 +25,21 @@ contract Device {
         _;
     }
 
+    modifier onlyCurrentNode() {
+        require(msg.sender == currentNode, "Only the currentNode can call this method.");
+        _;
+    }
+
+    modifier onlyElectorContract() {
+        require(msg.sender == electorContract, "Only the electorContract can call this method.");
+        _;
+    }
+
     function getNode() public view returns (address) {
         return currentNode;
     }
 
-    function setNode(address newNode) public onlyNodeContract {
+    function setNode(address newNode) public onlyElectorContract {
         currentNode = newNode;
     }
 
@@ -35,5 +53,9 @@ contract Device {
 
     function getStatus() public view returns (string memory) {
         return status;
+    }
+
+    function getPayment(uint256 amount) public onlyCurrentNode {
+        require(token.transferFrom(ownerContract, currentNode, amount), "Transfer failed.");
     }
 }
