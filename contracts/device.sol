ac4fbf78dfe7942a7ddbacc78d38a3cb0bca31ce
artamonovx <140037173+artamonovx@users.noreply.github.com>
2023-08-25 15:12:05 +0700

    feat: build contracts WIP

---
+++ contracts/device.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+contract Device {
+    string public dtype;
+    string public status;
+    string public version;
+    address public currentNode;
+    address public vendorContract;
+    address public electorContract;
+    address public nodeContract;
+    address public ownerContract;
+    IERC20 public token;
+
+    constructor(IERC20 _token) {
+        ownerContract = msg.sender;
+        token = _token;
+    }
+
+    modifier onlyNodeContract() {
+        require(msg.sender == nodeContract, "Only the nodeContract can call this method.");
+        _;
+    }
+
+    modifier onlyCurrentNode() {
+        require(msg.sender == currentNode, "Only the currentNode can call this method.");
+        _;
+    }
+
+    modifier onlyElectorContract() {
+        require(msg.sender == electorContract, "Only the electorContract can call this method.");
+        _;
+    }
+
+    function getNode() public view returns (address) {
+        return currentNode;
+    }
+
+    function setNode(address newNode) public onlyElectorContract {
+        currentNode = newNode;
+    }
+
+    function getType() public view returns (string memory) {
+        return dtype;
+    }
+
+    function getVersion() public view returns (string memory) {
+        return version;
+    }
+
+    function setStatus(string memory newStatus) public onlyCurrentNode {
+        status = newStatus;
+    }
+
+    function getStatus() public view returns (string memory) {
+        return status;
+    }
+
+    function getPayment(uint256 amount) public onlyCurrentNode {
+        require(token.transfer(currentNode, amount), "Transfer failed.");
+    }
+}
