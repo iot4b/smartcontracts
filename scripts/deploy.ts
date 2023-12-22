7412120f2608bce0f3768f9919ac1b89db471bdf
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2023-12-22 09:53:21 +0700

    elector

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,17 +7,21 @@ async function main() {
 
     const { contract: testContract } = await locklift.factory.deployContract({
         // name of your contract
-        contract: 'Test',
+        contract: 'Elector',
         // runtime deployment arguments
         constructorParams: {
-            data: "data"
+            defaultNodes: [
+                "0:b6f0dd040d1fa3f79871418040e7bb42c02dd674d32cc29fbc82f97dc119a212",
+                "0:e413df4ff5e475d6dfd78db61ea635d8eefb18753f3152c51b2929edd684ec57",
+                "0:c6bd6af3a4104c9cd3867c79cff5f62410cc95ca94e5e235c8303f9213f19582"
+            ]
         },
         // static parameters of contract
         initParams: {},
         // public key in init data
         publicKey: signer!.publicKey,
         // this value will be transfered from giver to deployable contract
-        value: toNano(1),
+        value: toNano(10),
     });
 
     console.log(testContract.address.toString());
