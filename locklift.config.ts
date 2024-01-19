d272a6b219f1dc6a59e13a09a82f2f2f05847f9f
artamonovx <ever@artamonovserg.ru>
2024-01-19 09:56:30 +0700

    ci: config

--- locklift.config.ts
+++ locklift.config.ts
@@ -45,16 +45,16 @@ const config: LockliftConfig = {
                 group: 'localnet',
                 type: 'graphql',
                 data: {
-                    endpoints: [process.env.LOCAL_NETWORK_ENDPOINT ?? ''],
+                    endpoints: [process.env.LOCAL_NETWORK_ENDPOINT],
                     latencyDetectionInterval: 1000,
                     local: true,
                 },
             },
             giver: {
-                address: '0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415',
+                address: process.env.LOCAL_GIVER_ADDRESS,
                 key: process.env.LOCAL_GIVER_KEY,
             },
-            tracing: { endpoint: process.env.LOCAL_NETWORK_ENDPOINT ?? '' },
+            tracing: { endpoint: process.env.LOCAL_NETWORK_ENDPOINT},
             keys: {
                 phrase: process.env.LOCAL_PHRASE,
                 amount: 20,
