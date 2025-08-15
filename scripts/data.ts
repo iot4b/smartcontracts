d0f01115e10091ffdfdf309041c33d028d285f4b
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2025-08-15 10:58:11 +0700

    remove secret keys

--- scripts/data.ts
+++ scripts/data.ts
@@ -4,7 +4,7 @@ export const data = {
         address: '0:9cb08c5c66f1607bb7fd6c75feca0d7b0902c22424a61c7a93a94302a9eea908',
         keyPair: {
             publicKey: 'f89efd172561f8d87ea3487409dd75656c304808a51afa43c6bf8e9b8ad11755',
-            secretKey: '6aa78366ae89bb30100ae5948dad3b5fb5984d6ddb369a815d4a576bd534d55b'
+            secretKey: ''
         }
     },
     Elector: {
@@ -12,7 +12,7 @@ export const data = {
         address: '0:09ff18e1396c3844c04cde933b1044e47b3b1e62c11a79572c4fd380d279084d',
         keyPair: {
             publicKey: '5ff5dfdb46b9fa5ac837cc3d346bf22ad54b286b4ece7edc7067bb7e8826ec90',
-            secretKey: 'e551b6212c7f103e92a6300e0058c669a815ae4e41185caf41ffd44b458218d1'
+            secretKey: ''
         }
     },
     Vendor: {
@@ -21,7 +21,7 @@ export const data = {
             address: '0:4b89819b5cdae07b1b97d7962bc5561b2f667f35d0e18cc9d426318a4edc97bd',
             keyPair: {
                 publicKey: '7cdb05984db3d2b6cf9230b4a7387e0deac7d6d2c1c976e72d1cf240d291f2f2',
-                secretKey: 'bf331db9c443e4a5671926c61973ba2ad8ffdf86da8edf01c34eb5a30618bb24'
+                secretKey: ''
             }
         },
         IOT4OpenWRT: {
@@ -29,7 +29,7 @@ export const data = {
             address: '0:041c0970f0de1f40a05f98e6659510fa6df85c78a45611f1b5a1635fa79dd9ab',
             keyPair: {
                 publicKey: '8ac5c9c635cd0c21d7c0da3cadacfd4b9c0f49f9a946cf434ea66a573afcb891',
-                secretKey: 'e6be1e2b2067d435cf2acb1f0ab71672abbe4a1780165b4c8ec9eac8af97e57c'
+                secretKey: ''
             }
         },
         IOT4Linux: {
@@ -37,7 +37,7 @@ export const data = {
             address: '0:9dd72e6259676545b8a4e3a957e34cb464dc080efe43d67e04970439d4e5b5ad',
             keyPair: {
                 publicKey: 'a396a9c919f560dfdf98a19127e9cc32013a9df029d209a364c22922f611e988',
-                secretKey: 'a53c1969823680ecf31901a5806e7e1ef2ccfb5fe15969567936b5248dbfcc56'
+                secretKey: ''
             }
         }
     },
@@ -47,7 +47,7 @@ export const data = {
             address: '0:a13c217c3dd65154e30165a018c8ac5615086de149983f9204388c2d2adc54df',
             keyPair: {
                 publicKey: '577389de782801ee317adf9ae1d4347422617a54689bbf3ff2c7cdab9b6fe78f',
-                secretKey: '6e8c7aed42a4711003555030b6430eb46d197b02d49ad08a59ed0c3bb361c2ca'
+                secretKey: ''
             }
         },
         OpenWRT: {
@@ -55,7 +55,7 @@ export const data = {
             address: '0:ac89285c69ebf4748d04a3a19bc8f831ef3cda7954d10a4c76465dcd06a82691',
             keyPair: {
                 publicKey: '5d8b742e6b6dc3edfec0bad4f3876e3d1fb1830018dae5348718f45e8bbf89c4',
-                secretKey: '56ed0ae97b9dce0dc3ab27b0f75eae732f31897ed6ceac8dd7cf400b294ef165'
+                secretKey: ''
             }
         },
         Linux: {
@@ -63,7 +63,7 @@ export const data = {
             address: '0:b3327142f0c73f703ff9aaccb9efc1cc66caffaab28b39b34db25c30d52a60c3',
             keyPair: {
                 publicKey: 'cee6db50ce76689f62be458599c419f6dffebc4f797df45052ca2f21839154a2',
-                secretKey: '0abe34e07f364c38887ba41f009a85d7fceaef7155ead86ed2418cb37f6e9f0d'
+                secretKey: ''
             }
         }
     },
@@ -73,7 +73,7 @@ export const data = {
             address: '0:45c31634fcce25dd6fd0cc66c1427390ad14dc77192a84124e3cdc96f45eb081',
             keyPair: {
                 publicKey: 'a6493ea99cfc7ed521eabc8f6b4afbd83d313d14e30002cc184d33335f1e09d7',
-                secretKey: 'f402603c7fc34cd05b3d36d36084746a2241b7205b9a5c5e068eff0375885bb3'
+                secretKey: ''
             }
         },
         Device2: {
@@ -81,7 +81,7 @@ export const data = {
             address: '0:352780d0e02e8903c71f3470d8a4b5b03b176bb87fb334de0eb043f23818b6d0',
             keyPair: {
                 publicKey: '8527e6475d80ee688fbf84bc1c54074fe51f037d9fc683c32e31b74341131878',
-                secretKey: '325a6dfe14c0e73fe81b75036e53be73e01bf8307fa0f781dbd7d85e374b3480'
+                secretKey: ''
             }
         },
         Device3: {
@@ -89,15 +89,15 @@ export const data = {
             address: '0:829c87bc400bc962462cf8cf8fa5d3d378a83f7151c8a78c848528d5416c8078',
             keyPair: {
                 publicKey: '5389801f7c4c9a47819085d8a18aebe5f7a51b810aa3e321d05a9790670e158d',
-                secretKey: '64f779460379f5498d8a8bd6b3ab78f9a35a32b6b91e0ca651161eb7bfbc52d6'
+                secretKey: ''
             }
         },
         KeeneticExtra: {
             name: 'Device',
-            address: '0:df44b8b9922d682068a1d11065812893bbbae28a075ca14604c5e1ef49d8cdd5',
+            address: '0:d8d8f3c3ab6912f281f12f02aa3ee417d9785914ce0457d599dedf018b2fa675',
             keyPair: {
-                publicKey: '605eb23bb224a3f9e0a0c7ef3d1a13803476ef59689308d9d2303f4f3b573b93',
-                secretKey: '48e78450cc695028f5b1c1b211ad6fd798b49e8550ff47183052bfaceff83759'
+                publicKey: '63aed2d9c1432568b80357827b07b784f6897ab4e8255fc8dec4582c504ff24a',
+                secretKey: ''
             }
         },
         KeeneticOmni: {
@@ -105,7 +105,7 @@ export const data = {
             address: '0:4bc91329ddb9baffeaf66a670bd92081b770074d77cf123e94396707e206eb2e',
             keyPair: {
                 publicKey: 'c4c33a030fa91905355123f2c016018541ec56b90a50690130e3cea673a0a149',
-                secretKey: 'fe25a73d4eb42e2dc4897da58a76a477149838276625049758d8859a8687b831'
+                secretKey: ''
             }
         }
     },
@@ -114,7 +114,7 @@ export const data = {
         address: '0:65a9a4033387a1472cd581ed76e4a8897fd3ec90a93d2e41f9262829e0325b84',
         keyPair: {
             publicKey: '7080f3df823176fc8630d7d35ece1940bb0bf8bf8369475b13b40cc7abeb73b2',
-            secretKey: '347fd06fc963925f448ca3c30c926ac17fc6959c7d433bf7f0f05cab7f5197b4'
+            secretKey: ''
         }
     },
     Node: {
@@ -123,7 +123,7 @@ export const data = {
             address: '0:d22c14be9a74264a51e8a399b4f347ddd9b1c3a703023d7091776969d39e6cd3',
             keyPair: {
                 publicKey: '1987a963de0ea40564f4479a3b55d61ef5d5eb5bdc60631f423dbed1058bd8a9',
-                secretKey: '4354d3cbc1af3fbe0d2cf954ee941948ad7bfde241e78a98a3417bcf64c826cd'
+                secretKey: ''
             }
         },
         Node2: {
@@ -131,7 +131,7 @@ export const data = {
             address: '0:a57d62d8e650e9ca3ef3299378a2875d68f8cc7bc68d830260d8a1155df06a92',
             keyPair: {
                 publicKey: '03b3d410a35ba9cda35ccee80f18dbaf62de1a5c727bab2cfcc287f8a5be427a',
-                secretKey: 'dff9f946bfaa2bddb06a1d092ec1004814efb5898428d13585de0c3d151032fe'
+                secretKey: ''
             }
         },
         Node3: {
@@ -139,7 +139,7 @@ export const data = {
             address: '0:c04a690d7b4e8d01802887a8e9abb9b0f74b5e207606cd92b4dbabf76ab7b0d9',
             keyPair: {
                 publicKey: '3790ecb012b5d890fa061b2b9f100aea1d407c1606615f86f04e0066d544c8fa',
-                secretKey: 'd2b4e14d1bb23b338304257b673c9f4a2c3be44b07a0e99117cb28f93feb4b0c'
+                secretKey: ''
             }
         }
     }
